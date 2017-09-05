3373a28c7bf587981007a0b8215dd8744667728c
Tobe O <thosakwe@gmail.com>
2017-09-05 03:37:32 +0000

    80

--- mediator_server/bin/run.sh
+++ mediator_server/bin/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 dname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dname/..
-sudo -E dart bin/force_https.dart &
+# sudo -E dart bin/force_https.dart &
 sudo -E dart bin/server.dart &
